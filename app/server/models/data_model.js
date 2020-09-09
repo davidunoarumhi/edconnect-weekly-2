3235ff0b47cff41ffb16efec88ac354ed1128abc
unknown <unodavid06@gmail.com>
2020-09-09 19:09:58 +0100

    Implementation of DataModel class

--- app/server/models/data_model.js
+++ app/server/models/data_model.js
@@ -8,7 +8,13 @@ class DataModel {
     }
 
     getById(id) {
-
+        //return the object with the specified id if such an object exists
+        let obj = this.data.find(obj => obj.id == id);
+        if (obj === undefined) {
+            return null;
+        } else {
+            return obj;
+        }
     }
 
     save(obj) {
@@ -20,11 +26,38 @@ class DataModel {
     }
 
     update(obj, id) {
-
+        // updates the properties of an object in the data array with the specified id
+        let item = this.data.find(item => item.id == id);
+        if (item === undefined) {
+            //item is equal to undefined when it cannot be found
+            return false;
+        }
+        else {
+            //loop over found item and obj passed in method and update properties with same property name
+            for (var x in item) {
+                for (var y in obj) {
+                    if (x === y){
+                      item[x] = obj[y];
+                    }
+                    else {
+                      continue;
+                    }
+                }
+            }
+            return true;
+        }
     }
 
     delete(id) {
-
+        //deletes the object with the specified id
+        var index = this.data.map(function(item) { return item.id; }).indexOf(id);
+        if (index > -1) {
+            // remove object
+            this.data.splice(index, 1);
+            return true;
+        } else if (index === -1) {
+            return false;
+        }
     }
 
     // this method will be overriden in the sub classes
